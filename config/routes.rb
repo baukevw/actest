5620d9750d1b8f494a1f6b869de79f4dfe5e28cf
baukevw <bauke@baukevanwieren.nl>
2021-01-27 11:00:15 +0100

    setup action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
